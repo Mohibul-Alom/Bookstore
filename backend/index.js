ba7603a6ae1fdd9360b5ee7fb0bd4f278488df4f
Mohibul <tuhin28012@gmail.com>
2021-09-14 01:01:59 +0200

    FEAT: base endpont with express working

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+
+const PORT = 5000;
+
+const app = express();
+const router = express.Router();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.use("/",router);
+
+
+router.get('/',(req,res,next) => {
+    res.status(200).json("Express funcionando correctamente");
+});
+
+//pages not found
+app.use('*',(req,res,next) => {
+    const error = new Error('Página no encontrada');
+    return res.status(404).json(error.message);
+});
+
+//error treatment
+app.use((error, req, res) => {
+    return res.status(error.status || 500).json(error.message || 'Unexpected error');
+});
+
+app.listen(PORT,() => {
+    console.log(`Server ejecutando en http://localhost:${PORT}`);
+})
