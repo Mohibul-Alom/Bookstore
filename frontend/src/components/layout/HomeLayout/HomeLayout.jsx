d1b4ea420375826e1692f406fecb24836471d4ea
Mohibul <tuhin28012@gmail.com>
2021-09-15 02:03:20 +0200

    FEAT: added front for book list

--- frontend/src/components/layout/HomeLayout/HomeLayout.jsx
+++ frontend/src/components/layout/HomeLayout/HomeLayout.jsx
@@ -1,36 +1,104 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-import { Header } from '../../sections';
-import {getBooks} from '../../../api/book.api';
+import { Header } from "../../sections";
+import { getBooks } from "../../../api/book.api";
+
+import {
+    Flex,
+    Spacer,
+    Box,
+    Stack,
+    Divider,
+    Heading,
+    SimpleGrid,
+    ButtonGroup,
+    Button,
+    IconButton,
+} from "@chakra-ui/react";
+import { AiFillEdit } from "react-icons/ai";
+import { BsBoxArrowUpRight, BsFillTrashFill } from "react-icons/bs";
 
 function HomeLayout() {
-
     const [books, setBooks] = useState([]);
 
-    useEffect(()=> {
+    useEffect(() => {
         getBooks()
-        .then(result => {
-            setBooks(result);
-            // console.log(result);
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    },[])
+            .then((result) => {
+                setBooks(result);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
     return (
-        <div>
-            <Header/>
-            <h1>HomeLayout</h1>
-            <ul>
-                {books.map(book =>(
-                    <li key={book._id}>
-                        {book.name}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+        <>
+            <Header />
+            <Flex
+                w="full"
+                bg="gray.800"
+                p={50}
+                alignItems="center"
+                justifyContent="center"
+            >
+                <Stack direction={{ base: "column" }} w="full" shadow="dark-lg">
+                    <SimpleGrid
+                        spacingY={3}
+                        columns={{ base: 1, md: 4 }}
+                        w={{ base: 120, md: "full" }}
+                        textTransform="uppercase"
+                        py={{ base: 1, md: 4 }}
+                        px={{ base: 4, md: 10 }}
+                        fontSize="md"
+                        fontWeight="hairline"
+                        display="table-header-group"
+                    >
+                        {/* <span mt="2">Books Name</span> */}
+                        <Heading as="h3" size="lg">
+                            Books
+                        </Heading>
+                    </SimpleGrid>
+
+                    {books.map((book) => {
+                        return (
+                            <>
+                                <Flex direction={{ base: "row", xxs: "column" }} key={book._id}>
+                                    <SimpleGrid
+                                        spacingY={3}
+                                        columns={{ base: 3, md: 4 }}
+                                        w="full"
+                                        py={2}
+                                        px={12}
+                                        fontWeight="hairline"
+                                    >
+                                        <span>{book.name}</span>
+                                        <Spacer />
+                                        <Spacer />
+
+                                        <Flex justify={{ md: "end" }}>
+                                            <ButtonGroup variant="solid" size="sm" spacing={3}>
+                                                <IconButton
+                                                    colorScheme="blue"
+                                                    icon={<BsBoxArrowUpRight />}
+                                                />
+                                                <IconButton colorScheme="green" icon={<AiFillEdit />} />
+                                                <IconButton
+                                                    colorScheme="red"
+                                                    variant="outline"
+                                                    icon={<BsFillTrashFill />}
+                                                />
+                                            </ButtonGroup>
+                                        </Flex>
+                                    </SimpleGrid>
+                                </Flex>
+                                <Divider />
+                            </>
+                        );
+                    })}
+                </Stack>
+            </Flex>
+        </>
+    );
 }
 
-export default HomeLayout
+export default HomeLayout;
